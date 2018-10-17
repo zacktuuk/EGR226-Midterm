b822ada8c9e1aa1195ba748b5e952347dace3920
Zack Tuuk <Zack Tuuk@35.40.168.79>
2018-10-17 11:15:22 -0400

    Project Files

---
+++ EGR226-902_Tuuk_Speet_MidtermProject/Debug/main.d
@@ -0,0 +1,49 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv8/ccs_base/arm/include/msp.h
+main.obj: C:/ti/ccsv8/ccs_base/arm/include/msp432p401r.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_types.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_types.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_stdint.h
+main.obj: C:/ti/ccsv8/ccs_base/arm/include/msp_compatibility.h
+main.obj: C:/ti/ccsv8/ccs_base/arm/include/msp432p401r_classic.h
+main.obj: C:/ti/ccsv8/ccs_base/arm/include/CMSIS/core_cm4.h
+main.obj: C:/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+main.obj: C:/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+main.obj: C:/ti/ccsv8/ccs_base/arm/include/system_msp432p401r.h
+main.obj: ../LCDMENU.txt
+main.obj: ../LCD_INIT.txt
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdio.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/_ti_config.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/linkage.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdarg.h
+main.obj: ../MENU_KEYPAD.txt
+
+../main.c: 
+C:/ti/ccsv8/ccs_base/arm/include/msp.h: 
+C:/ti/ccsv8/ccs_base/arm/include/msp432p401r.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_stdint.h: 
+C:/ti/ccsv8/ccs_base/arm/include/msp_compatibility.h: 
+C:/ti/ccsv8/ccs_base/arm/include/msp432p401r_classic.h: 
+C:/ti/ccsv8/ccs_base/arm/include/CMSIS/core_cm4.h: 
+C:/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+C:/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+C:/ti/ccsv8/ccs_base/arm/include/system_msp432p401r.h: 
+../LCDMENU.txt: 
+../LCD_INIT.txt: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdio.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/_ti_config.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/linkage.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdarg.h: 
+../MENU_KEYPAD.txt: 
